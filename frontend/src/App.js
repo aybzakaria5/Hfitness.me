61cf3ab8b43240f8d5f3e98a812a164ebb87a8e9
Layla Abkari <99873278+Lelaabk@users.noreply.github.com>
2024-02-28 11:21:29 +0000

    Starting React

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header'; 
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
+function App() {
+  return (
+    function App() {
+        return (
+          <div>
+            <Header />
+            <h1>Your Main Content</h1>
+          </div>
+        );
+      }
+  );
+}
+
+export default App;
