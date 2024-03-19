6b0d677f5047572bca0812bc8678795d93f6abfa
Layla Abkari <99873278+Lelaabk@users.noreply.github.com>
2024-03-19 14:56:51 +0000

    Footer section ended

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Reasons from './components/Reasons/Reasons';
 import Plans from './components/Plans/Plans';
 import Testimonials from './components/Testimonials/Testimonials';
 import Join from './components/Join/Join';
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Plans/>
       <Testimonials/>
       <Join/>
+      <Footer/>
     </div>
   );
 }
